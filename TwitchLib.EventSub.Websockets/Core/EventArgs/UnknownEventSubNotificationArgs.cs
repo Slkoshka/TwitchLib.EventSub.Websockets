00871ba5f5b7b75b2f5118885f9835f4438e2b8e
AoshiW <48525283+AoshiW@users.noreply.github.com>
2025-07-24 08:42:27 +0200

    add handling for unknow events (#64)
    add handling for unknow events

---
+++ TwitchLib.EventSub.Websockets/Core/EventArgs/UnknownEventSubNotificationArgs.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json;
+using TwitchLib.EventSub.Websockets.Core.Models;
+
+namespace TwitchLib.EventSub.Websockets.Core.EventArgs;
+
+public class UnknownEventSubNotificationArgs : TwitchLibEventSubEventArgs<EventSubNotification<JsonElement>>
+{ }
---
+++ TwitchLib.EventSub.Websockets/Handler/UnknownEventSubNotificationHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using TwitchLib.EventSub.Websockets.Core.EventArgs;
+using TwitchLib.EventSub.Websockets.Core.Models;
+
+namespace TwitchLib.EventSub.Websockets.Handler;
+
+class UnknownEventSubNotificationHandler : NotificationHandler<UnknownEventSubNotificationArgs, EventSubNotification<JsonElement>>
+{
+    /// <inheritdoc />
+    public override string SubscriptionType => string.Empty;
+
+    /// <inheritdoc />
+    public override string SubscriptionVersion => string.Empty;
+
+    /// <inheritdoc />
+    public override string EventName => nameof(EventSubWebsocketClient.UnknownEventSubNotification);
+}
